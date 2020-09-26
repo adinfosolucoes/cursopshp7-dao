a4b1578f5474c377ec00a4e81d47583adc66fc3d
Daniel Gomes <danielgs.dgs@gmail.com>
2020-09-26 00:01:10 -0300

    Metodos de lista

--- index.php
+++ index.php
@@ -9,4 +9,25 @@
 
 	echo $root;
 
+	echo "<hr/>";
+
+
+	$lista = Usuario::getList();
+
+	echo json_encode($lista);
+
+
+	echo "<hr/>";
+
+	$search = Usuario::search("da");
+	echo json_encode($search);
+
+	echo "<hr/>";
+
+	$usuario = new usuario();
+
+	$usuario->login("Fernanda", "amorekito06");
+
+	echo $usuario;
+
 ?>
